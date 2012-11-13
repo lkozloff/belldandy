b6194815350ca8cf5ab8da5fc4f77bb021b6a2aa
root <root@raspberrypi.(none)>
2012-11-13 13:54:00 +0700

    first commit

---
+++ process.php
@@ -0,0 +1,37 @@
+<?php
+	$command = "/usr/bin/mpg321";
+	$user = "root";
+	$source_dir = "/var/www/";
+	$schedules=simplexml_load_file('bells.xml');
+
+	foreach($schedules as $schedule){
+	if($schedule->active =='true'){
+	   print("### ".$schedule->title." ###\n");
+
+	   foreach($schedule->periods->period as $period){
+		$name = $period->name;
+		$start = $period->starttime;
+		$end = $period->endtime;
+		$dow = $period->dow;
+		$sound = $period->sound;
+		
+		$stime = explode(":",$start);
+		$etime = explode(":",$end);
+		
+		$shour = $stime[0];
+		$sminute = $stime[1];
+		
+		$ehour = $etime[0];
+		$eminute = $etime[1];
+		
+		print("\n    #- $name -#\n");
+		print("$sminute $shour * * $dow $user\t $command $source_dir$sound\n");
+		print("$eminute $ehour * * $dow $user\t $command $source_dir$sound\n");
+
+	   }
+	   print("\n");
+	   }
+	}
+
+
+?>
---
+++ write.php
@@ -0,0 +1,59 @@
+<?php
+	if(!isset($_POST['submit'])){
+	   $title = "Fun Test!";
+	   $active = "true";
+	   $pname = "P1";
+    	   $starthour = 18;
+    	   $startmin = 15;
+    	   $endhour = 18;
+    	   $endmin = 16;
+    	   $dow_arr = array('MON','TUE','WED','THU','FRI');
+    	   $sound = "bell.mp3";
+	}
+
+	else{
+	   $title = $_POST['title'];
+	   $active = $_POST['active'];
+	   $pname = $_POST['pname'];
+	   $starthour = $_POST['starthour'];
+	   $startmin = $_POST['startmin'];
+	   $endhour = $_POST['starthour'];
+	   $endmin = $_POST['endmin'];
+	   $sound = $_POST['sound'];
+	   $dow_arr = $_POST['dow_arr'];
+
+	}
+
+	//construct a nice DOW string
+	foreach($dow_arr as $day){
+	   $dow.=$day.",";
+	}
+	print($sound);
+
+	//get rid of the trailing comma
+	$dow = rtrim($dow,",");
+
+
+
+	$schedules=simplexml_load_file('bells.xml');
+
+	$schedule=$schedules->addChild('schedule');
+
+	$schedule->addChild('title',$title);
+	$schedule->addChild('active',$active);
+	$periods=$schedule->addChild('periods');
+		
+    $period = $periods->addChild('period');
+
+   	$period->addChild('name',$pname);
+    $period->addChild('starttime',$starthour.":".$startmin);
+    $period->addChild('endtime',$endhour.":".$endmin);
+    $period->addChild('dow',$dow);
+    $period->addChild('sound',$sound);
+	
+    $handle=fopen("bells.xml","wb");
+    fwrite($handle,$schedules->asXML());
+    fclose($handle);
+
+	//echo $schedules->asXML();
+?>
